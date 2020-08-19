22b909efb8622ecbcc987c555532c6a8f6fbe67d
medxkh <medkh6699@gmail.com>
2020-08-19 18:36:42 +0200

    add scrool content

--- scrape.py
+++ scrape.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("https://french.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=3d+printer&viewtype=&tab=")
+time.sleep(2)
+driver.execute_script("window.scrollTo(0,document.body.scrollHeigh)")
+time.sleep(2)
+elem = driver.find_elements_by_css_selector("p")
+for elems in elem:
+    driver.find_elements_by_class_name("elements-title-normal__content large")
+    print(elems.text)
+
+
 driver.close()
